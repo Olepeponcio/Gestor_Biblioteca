39277079ab037aea2426245fd1b8bcae346ad3b4
Olepeponcio <olepepinsan@gmail.com>
2025-09-21 23:17:52 +0200

    testing

--- main.py
+++ main.py
@@ -60,14 +60,20 @@ menú │
 │ Salir      │
 └────────────┘
 """
-from autor import Autor
-from libro_fisico import LibroFisico
-from libro_digital import LibroDigital
-from revista import Revista
-from tipo_item import TipoItem
-
-autor1 = Autor("Gabriel", "Garcia Marquez", "Colombiana")
-autor2  = Autor("Julio", "Cortazar", "Belga")
-autores = [autor1, autor2]
-var1 = LibroFisico("pasillo e", "00000", "100 anos de soledad", TipoItem.FISICO, autores)
-print(var1.__str__())
+# from autor import Autor
+# from libro_fisico import LibroFisico
+# from libro_digital import LibroDigital
+# from revista import Revista
+# from tipo_item import TipoItem
+#
+# autor1 = Autor("Gabriel", "Garcia Marquez", "Colombiana")
+# autor2  = Autor("Julio", "Cortazar", "Belga")
+# autores = [autor1, autor2]
+# var1 = LibroFisico("pasillo e", "00000", "100 anos de soledad", TipoItem.FISICO, autores)
+# print(var1.__str__())
+#
+# var2 = LibroDigital("00000", "100 anos de soledad", TipoItem.FISICO, autores, ".pdf", 12.5)
+# print(var2.__str__())
+#
+# var3 = Revista("00000", "100 anos de soledad", TipoItem.FISICO, autores, 1, "trimestral" )
+# print(var3.__str__())
